77c50cd402ada790779ccda7df73e0c69f38a038
Ameer Dawood <ameer1234567890@gmail.com>
2017-07-04 15:39:51 +0500

    Create tinifier.sh

---
+++ tinifier.sh
@@ -0,0 +1,10 @@
+api_key="`cat ../.tinify_api_key`"
+for file in `ls files`; do
+  echo "Compressing $file...."
+  response="`curl --user api:$api_key --data-binary @files/$file --output api_response.txt -i https://api.tinify.com/shrink`"
+  download_url="`cat api_response.txt | grep location | awk '{print $2}'`"
+  curl $download_url --user api:$api_key --header "Content-Type: application/json" --data '{ "preserve": ["location", "creation"] }' --output compressed/$file
+  echo "Done compressing $file"
+  rm api_response.txt
+done
+echo "All files compressed!"
