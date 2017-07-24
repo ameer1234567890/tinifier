980ce6af97d22e46b0414030bd555723f7a4fe9c
Ameer Dawood <ameer1234567890@gmail.com>
2017-07-24 13:38:47 +0500

    Fix API Limit bug

--- tinifier.sh
+++ tinifier.sh
@@ -1,3 +1,4 @@
+API_LIMIT="500"
 api_key="`cat ../.tinify_api_key`"
 mkdir -p compressed
 echo "`date +'%Y/%m/%d %H:%M:%S'` Starting compression...."
@@ -10,12 +11,12 @@ for file in `ls files`; do
   response="`curl --progress-bar --user api:$api_key --data-binary @files/$file --output api_response.txt -i https://api.tinify.com/shrink`"
   download_url="`cat api_response.txt | grep location | awk '{print $2}'`"
   compression_count="`cat api_response.txt | grep compression-count | awk '{print $2}'`"
-  echo "`date +'%Y/%m/%d %H:%M:%S'` Remaining API Requests: $compression_count"
+  echo "`date +'%Y/%m/%d %H:%M:%S'` Total API Requests: $compression_count"
   curl $download_url  --progress-bar --user api:$api_key --header "Content-Type: application/json" --data '{ "preserve": ["location", "creation"] }' --output compressed/$file
   new_size="`expr $(stat --printf="%s" compressed/$file) / 1024`"
   echo "`date +'%Y/%m/%d %H:%M:%S'` Done compressing $file (${new_size}KB)"
   rm api_response.txt
-  if [ "compression_count" -lt 1 ]; then
+  if [ "compression_count" -gt "`expr $API_LIMIT - 1`" ]; then
     echo "`date +'%Y/%m/%d %H:%M:%S'` API Limit Reached! Exiting...."
     exit 1
   fi
